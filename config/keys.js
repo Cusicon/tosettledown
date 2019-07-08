c49a75ffccb3d7a9c44e1e74fdf3c483d1fb1fcc
Success Chukwu <chuxsuccess0@gmail.com>
2019-07-08 22:35:08 +0100

    [Keys]. Changes made.

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+//-- All keys to this project lives Here.
+
+module.exports = {
+    auth: {
+        google: {
+            clientID: '154146660831-rms4t4kuj2ltpj5ct3g03b04g4akn6tc.apps.googleusercontent.com',
+            clientSecret: '0DYh4lfyKv24ePYADtP0bFTh'
+        }
+    },
+    mongodb: {
+        localURI: 'mongodb://localhost/tosettledown',
+        onlineURI: 'mongodb://success:0123456789jehova.@ds135433.mlab.com:35433/heroku_5m6mvm1g',
+    }
+}
