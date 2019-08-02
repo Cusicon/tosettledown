c86cdbc83ee085c6da0967d1795a46d4b9042b59
Success <Success>
2019-08-02 22:48:34 +0100

    Added the edit profile page to profile section.

--- routes/app/menu/profile.js
+++ routes/app/menu/profile.js
@@ -18,6 +18,22 @@ router.get("/:username", (req, res) => {
     });
 });
 
+// Update users
+router.get("/update/:username", (req, res) => {
+    var username = req.params.username;
+    User.getUserByUsername(username, (err, profile_user) => {
+        if (err) console.log(err);
+        else {
+            res.render('./app/menu/profile', {
+                title: `${profile_user.fullname.firstname}'s profile`,
+                profile_user: profile_user
+            });
+        }
+    });
+});
+
+
+
 router.get("/", (req, res) => res.redirect("/app/encounters"));
 
 module.exports = router;
