c0266d5428806d673417673504f3dc29db052f0f
wizguy007 <akinwizguy@gmail.com>
2019-08-13 05:26:32 +0100

    arrange somestuff for chat page

--- public/lib/scripts/app/chat.js
+++ public/lib/scripts/app/chat.js
@@ -42,3 +42,37 @@ $(document).on('ready', () => {
     }
 
 });
+
+
+(function init () {
+    let observe;
+    if (window.attachEvent) {
+        observe = function (element, event, handler) {
+            element.attachEvent('on'+event, handler);
+        };
+    }
+    else {
+        observe = function (element, event, handler) {
+            element.addEventListener(event, handler, false);
+        };
+    }
+
+    let text = document.getElementById('message-input');
+    function resize () {
+        text.style.height = 'auto';
+        text.style.height = text.scrollHeight+'px';
+    }
+    /* 0-timeout to get the already changed text */
+    function delayedResize () {
+        window.setTimeout(resize, 0);
+    }
+    observe(text, 'change',  resize);
+    observe(text, 'cut',     delayedResize);
+    observe(text, 'paste',   delayedResize);
+    observe(text, 'drop',    delayedResize);
+    observe(text, 'keydown', delayedResize);
+
+    text.focus();
+    text.select();
+    resize();
+})();
