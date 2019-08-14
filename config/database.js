5e011ad0228605738bd50c204849845d98eda1eb
Success <Success>
2019-08-14 15:51:30 +0100

    Database commit 3.

--- config/database.js
+++ config/database.js
@@ -1,7 +1,3 @@
 module['exports'] = {
-<<<<<<< HEAD
     'uri': get_env('MONGOLAB_BLUE_URI', 'mongodb://localhost/tosettledown'),
-=======
-    'uri': get_env('MONGOLAB_BLUE_URI', 'mongodb://localhost/tosettledown')
->>>>>>> createUser_Uploads
 };
