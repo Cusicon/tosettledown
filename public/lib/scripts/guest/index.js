ba1fe7fe55aa48dff78e6bb3d37bd3ec1937a0f5
Success <Success>
2019-08-31 03:31:18 +0100

    Changes in Index HTML

--- public/lib/scripts/guest/index.js
+++ public/lib/scripts/guest/index.js
@@ -5,22 +5,22 @@ function toggleSignIn() {
         loginBtn = $('#loginBtn');
     loginBtn.click(() => {
         if (loginBtn.text().toLowerCase() == "sign in") {
-            loginBtn.text("Sign up");
+            loginBtn.text("Sign Up");
             loginBtn.attr("href", "#loginForm");
             loginForm.css({ "left": "0%" });
         } else {
-            loginBtn.text("Sign in");
+            loginBtn.text("Sign In");
             loginBtn.attr("href", "#regForm");
             loginForm.css({ "left": "-100%" });
         }
     });
 
     if (location.href.includes("#regForm")) {
-        loginBtn.text("Sign in");
+        loginBtn.text("Sign In");
         loginBtn.attr("href", "#loginForm");
         loginForm.css({ "left": "-100%" });
     } else if (location.href.includes("#loginForm")) {
-        loginBtn.text("Sign up");
+        loginBtn.text("Sign Up");
         loginBtn.attr("href", "#regForm");
         loginForm.css({ "left": "0%" });
     }
